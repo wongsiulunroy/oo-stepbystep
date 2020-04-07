b0a9d9af84aca4d4b254c5710815e9f306dd06af
wongro2 <roy.wong@oocl.com>
2020-04-07 15:47:45 +0800

    update Student and studentTest

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,12 +1,21 @@
 public class Student extends Person {
+    private String classNo;
 
-
-    public Student(String name, int age) {
+    public Student(String name, int age, String classNo) {
         super(name, age);
+        this.classNo = classNo;
+    }
+
+    public String getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo(String classNo) {
+        this.classNo = classNo;
     }
 
     @Override
     String introduce(){
-        return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL",getName(),getAge());
+        return String.format("My name is %s. I am %s years old. I am a student of %s. Coding for the glory of OOCL",getName(),getAge(),classNo);
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 public class StudentTest {
     @Test
     public void should_return_student_introduce(){
-        Person student = new Student("Tom", 21);
-        assertEquals("My name is Tom. I am 21 years old. Coding for the glory of OOCL",student.introduce());
+        Person student = new Student("Tom", 21,"Class 2");
+        assertEquals("My name is Tom. I am 21 years old. I am a student of Class 2. Coding for the glory of OOCL",student.introduce());
     }
 
 }
